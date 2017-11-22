542a6abeded768237ef8ece03c7628e9808b0b84
Yuki Nishijima <yk.nishijima@gmail.com>
2017-11-21 21:04:30 -0500

    Do not allow to create components using reserved names

--- lib/generators/stencil/component/component_generator.rb
+++ lib/generators/stencil/component/component_generator.rb
@@ -4,13 +4,19 @@ module Stencil
   class ComponentGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
+    RESERVED_ELEMENT_NAMES = %w(
+      annotation-xml
+      color-profile
+      font-face
+      font-face-src
+      font-face-uri
+      font-face-format
+      font-face-name
+      missing-glyph
+    ).freeze
+
     def create_component
-      if !file_name.include?('-')
-        raise "Invalid custom element name `#{file_name}'.\n\n" \
-              "The custom element name must have at least one dash '-' in its name by specification. " \
-              "For the exact requirements, please refer to W3C's official documenattion:\n\n" \
-              "    https://w3c.github.io/webcomponents/spec/custom/#valid-custom-element-name\n\n"
-      end
+      validate_element_name!
 
       template "component.tsx",     "app/javascript/components/#{file_name}/#{file_name}.tsx"
       template "component.scss",    "app/javascript/components/#{file_name}/#{file_name}.scss"
@@ -32,6 +38,23 @@ JSON
 
     def class_name # :doc:
       @_class_name ||= super.underscore.classify
+    end
+
+    def validate_element_name!
+      if !file_name.include?('-')
+        raise "Invalid custom element name `#{file_name}'.\n\n" \
+              "The custom element name must have at least one dash '-' in its name by specification. " \
+              "For the exact requirements, please refer to W3C's official documentation:\n\n" \
+              "    https://w3c.github.io/webcomponents/spec/custom/#valid-custom-element-name\n\n"
+      end
+
+      if RESERVED_ELEMENT_NAMES.include?(file_name)
+        raise "Invalid custom element name `#{file_name}'.\n\n" \
+              "The name `#{file_name}' is a reserved element name and can't be overwritten. Reserved names are the followings:\n\n" \
+              "    #{RESERVED_ELEMENT_NAMES.join(", ")}\n\n" \
+              "For the exact requirements, please refer to W3C's official documentation:\n\n" \
+              "    https://w3c.github.io/webcomponents/spec/custom/#valid-custom-element-name\n\n"
+      end
     end
   end
 end
