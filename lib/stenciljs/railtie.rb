cb873be4a7db9b89f4be529889280e7353d1fcc9
Yuki Nishijima <yk.nishijima@gmail.com>
2017-11-21 20:51:53 -0500

    Remove unneeded namespace

--- lib/stenciljs/railtie.rb
+++ lib/stenciljs/railtie.rb
@@ -1,15 +1,13 @@
 require "rails/railtie"
 
 module Stenciljs
-  class Rails
-    class Railtie < ::Rails::Railtie
-      initializer :stenciljs, group: :all do |app|
-        app.config.assets.paths += %w(public/components/build/)
-      end
+  class Railtie < ::Rails::Railtie
+    initializer :stenciljs, group: :all do |app|
+      app.config.assets.paths += %w(public/components/build/)
+    end
 
-      rake_tasks do
-        load "tasks/stencil.rake"
-      end
+    rake_tasks do
+      load "tasks/stencil.rake"
     end
   end
 end
